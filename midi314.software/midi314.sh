c52d25c5bbb1a42e2eebcec19325c7bc5cca2af9
Guillaume Savaton <guillaume@baierouge.fr>
2018-03-26 22:50:26 +0200

    Added Jack, Sooperlooper, Fluidsynth scripts and configs

---
+++ midi314.software/midi314.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+qjackctl --start --active-patchbay=midi314.qjackctl &
+
+fluidsynth --server --no-shell --audio-driver=jack /usr/share/sounds/sf2/TimGM6mb.sf2 &
+
+slgui --load-session=midi314.slsess --load-midi-binding=midi314.slb
+
+killall fluidsynth
