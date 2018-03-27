d4c7ead27304e268cbc4ac0aa6da3e16c208003c
Guillaume Savaton <guillaume@baierouge.fr>
2018-03-27 22:21:21 +0200

    Refactoring and fixes

--- midi314.software/midi314.sh
+++ midi314.software/midi314.sh
@@ -2,7 +2,7 @@
 
 qjackctl --start --active-patchbay=midi314.qjackctl &
 
-fluidsynth --server --no-shell --audio-driver=jack /usr/share/sounds/sf2/TimGM6mb.sf2 &
+fluidsynth --server --no-shell --dump --audio-driver=jack /usr/share/sounds/sf2/TimGM6mb.sf2 &
 
 slgui --load-session=midi314.slsess --load-midi-binding=midi314.slb
 
