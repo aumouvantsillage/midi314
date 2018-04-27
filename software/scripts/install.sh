aabc3389ed5ebc749699448e53868fda08d54af5
Guillaume Savaton <guillaume@baierouge.fr>
2018-04-27 22:04:51 +0200

    Fixed source dirs in install script

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
 PREFIX=${PREFIX:-/usr}
 BIN=$PREFIX/bin
 SHARE=$PREFIX/share/midi314
 
-cp software/midi314-display/target/release/midi314-display $BIN
-cp software/midi314-looper/target/release/midi314-looper $BIN
-cp software/scripts/midi314.sh $BIN
+cp $DIR/../midi314-display/target/release/midi314-display $BIN
+cp $DIR/../midi314-looper/target/release/midi314-looper   $BIN
+cp $DIR/../scripts/midi314.sh                             $BIN
 
 mkdir -p $SHARE
-cp software/scripts/midi314.qjackctl $SHARE
-cp software/scripts/midi314.rules $SHARE
+cp $DIR/midi314.qjackctl $SHARE
+cp $DIR/midi314.rules    $SHARE
